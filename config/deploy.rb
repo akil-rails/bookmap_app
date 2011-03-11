c5fdf5b04ad4ee144994976f88454637064b2ea2
akhilesh <kataria.subs@gmail.com>
2011-03-11 14:24:24 +0530

    fixed reaper not found error by adding override

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,3 +29,12 @@ namespace :deploy do
   end
   after "deploy:update_code", "deploy:copy_database_configuration"
 end
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
