ee66354caa98a8043ae91db7ca40b2b7b1495a8e
akhilesh <kataria.subs@gmail.com>
2011-04-12 21:03:49 +0530

    separate markers depending on the type of store

--- app/helpers/stores_helper.rb
+++ app/helpers/stores_helper.rb
@@ -1,2 +1,5 @@
 module StoresHelper
+  def store_content(store)
+    link_to (store.name, store)
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,11 @@
 class Member < ActiveRecord::Base
   belongs_to :store
+  
+  def avid?
+    plan_id == 40
+  end
+  
+  def in_waiver?
+    plan_id == 27
+  end
 end
