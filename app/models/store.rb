572ecdd150b922b92f220ff24ad4eb0bb5eafcac
akhilesh <kataria.subs@gmail.com>
2011-05-26 01:00:51 +0530

    fixed silly loop errors

--- app/models/store.rb
+++ app/models/store.rb
@@ -6,7 +6,7 @@ class Store < ActiveRecord::Base
   def compute_member_distance_from_store    
     members_needing_computation = members.select{ |m| m.distance_from_store == 0 }
     
-    while (mems = members_needing_computation.slice!(1, 50))
+    while ((mems = members_needing_computation.slice!(0, 50)).length > 0)
       query = "destinations=#{lat},#{lng}&origins="
       mems.each do |m|       
           query << "#{m.lat},#{m.lng}|"
@@ -18,14 +18,16 @@ class Store < ActiveRecord::Base
       data = resp.body
 
       result = ActiveSupport::JSON.decode(data)
-      unless result['rows'].nil?
+      puts "#{result} - #{mems.length} - #{members_needing_computation.length}"
+      if ( result['status'] == "OK" )
         result['rows'].each_with_index do |r, i|
-          puts mems[i]
-          mems[i].distance_from_store = r['elements'][0]['distance']['text'].gsub(/[a-z ]/,'')
-          mems[i].save
+          if ( r['elements'][0]['status'] == "OK" )
+            mems[i].distance_from_store = r['elements'][0]['distance']['text'].gsub(/[a-z ]/,'')
+            mems[i].save
+          end
         end
       else
-        result
+        puts result['status']
       end
     end
   end
